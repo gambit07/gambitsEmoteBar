729b49d37b25f46ec237f66dffa518fd4a86eea7
Gambit <kbelite8@gmail.com>
2025-03-05 00:53:36 -0500

    1.0.7

---
+++ scripts/module.js
@@ -0,0 +1,19 @@
+import { generateEmotes } from './emotePreview.js';
+
+Hooks.once('init', async function() {
+	Hooks.on('getSceneControlButtons', (controls) => {
+	const sidebarControls = controls.find(control => control.name === "token");
+
+	if (sidebarControls) {
+		sidebarControls.tools.push({
+		name: "emote-bar",
+		title: "Emote Bar",
+		icon: "fas fa-face-smile",
+		onClick: async () => {
+			await generateEmotes();
+		},
+		button: true
+		});
+	}
+	});
+});
