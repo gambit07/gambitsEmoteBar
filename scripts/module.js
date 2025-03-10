990096992e3b321acb196760ea7b1d4213f285a5
Gambit <kbelite8@gmail.com>
2025-03-09 21:44:38 -0400

    1.0.15

--- scripts/module.js
+++ scripts/module.js
@@ -1,41 +1,8 @@
 import { generateEmotes } from './emoteHandler.js';
+import { registerSettings } from './settings.js';
 
 Hooks.once('init', async function() {
-	game.settings.register("gambitsEmoteBar", "emoteSoundPaths", {
-		name: "Emote File Paths",
-		scope: "world",
-		config: false,
-		type: Object,
-		default: {}
-	});
-
-	game.settings.register("gambitsEmoteBar", "emoteSoundEnable", {
-		name: "Enable Emote Sounds",
-		hint: "Emote sounds are configurable within the Emote Bar and the GM can set default paths.",
-		scope: "world",
-		config: true,
-		type: Boolean,
-		default: true
-	});
-
-	game.settings.register("gambitsEmoteBar", "emoteSoundEnablePerUser", {
-		name: "Enable Emote Sounds Per User",
-		hint: "With this setting enabled, users are able to customize emote sound paths on a per-user basis which will take precedence over the GM's path.",
-		scope: "world",
-		config: true,
-		type: Boolean,
-		default: false
-	});
-
-	game.settings.register("gambitsEmoteBar", "emoteSoundDefaultPath", {
-		name: "Default Sounds Path",
-		hint: "If set, the FilePicker will use this default path for audio files instead of the root.",
-		scope: "world",
-		config: true,
-		restricted: true,
-		type: String,
-		default: "/"
-	  });
+	registerSettings();
 
 	if (!game.gambitsEmoteBar) {
 		game.gambitsEmoteBar = { dialogOpen: false, dialogInstance: null, loveActive: new Map() };
@@ -75,20 +42,4 @@ Hooks.once('init', async function() {
 		});
 	}
 	});
-
-	Sequence.prototype.applyEmoteSound = function(emote) {
-		const moduleSoundPaths = game.settings.get("gambitsEmoteBar", "emoteSoundPaths") || {};
-		let soundPath = moduleSoundPaths[emote] || "";
-		
-		const userOverrides = game.user.getFlag("gambitsEmoteBar", "emoteSoundOverrides") || {};
-		soundPath = userOverrides[emote] || soundPath;
-		
-		if (soundPath && soundPath.trim() !== "") {
-		  this.sound()
-			.file(soundPath)
-			.fadeInAudio(500)
-			.fadeOutAudio(500);
-		}
-		return this;
-	  };
 });
---
+++ scripts/settings.js
@@ -0,0 +1,37 @@
+export function registerSettings() {
+    game.settings.register("gambitsEmoteBar", "emoteSoundPaths", {
+        name: "Emote File Paths",
+        scope: "world",
+        config: false,
+        type: Object,
+        default: {}
+    });
+
+    game.settings.register("gambitsEmoteBar", "emoteSoundEnable", {
+        name: "Enable Emote Sounds",
+        hint: "Emote sounds are configurable within the Emote Bar and the GM can set default paths.",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: true
+    });
+
+    game.settings.register("gambitsEmoteBar", "emoteSoundEnablePerUser", {
+        name: "Enable Emote Sounds Per User",
+        hint: "With this setting enabled, users are able to customize emote sound paths on a per-user basis which will take precedence over the GM's path.",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false
+    });
+
+    game.settings.register("gambitsEmoteBar", "emoteSoundDefaultPath", {
+        name: "Default Sounds Path",
+        hint: "If set, the FilePicker will use this default path for audio files instead of the root.",
+        scope: "world",
+        config: true,
+        restricted: true,
+        type: String,
+        default: "/"
+    });
+}
